721ad3ff323aff003672324746d0024b5adcf8d6
Brenda  Mukindia <mukindiabrenda67@gmail.com>
2024-07-31 04:29:46 +0300

    adding requirement.txts

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -14,3 +14,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
+
+
+
+
+
+
+
+
+
+
+
